bd4b1afa986bd9ef399f234700747dcceaeb3e70
Mihkel <mgdrobshev@gmail.com>
2019-08-25 15:44:40 +0300

    sdfsa

---
+++ test.py
@@ -0,0 +1,26 @@
+
+
+import speedtest
+
+
+def test():
+    s = speedtest.Speedtest()
+    s.get_servers()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    res = s.results.dict()
+    return res["download"], res["upload"], res["ping"]
+
+
+def main():
+    # write to csv
+    with open('file.csv', 'w') as f:
+        f.write('download,upload,ping\n')
+        for i in range(3):
+            print('Making test #{}'.format(i+1))
+            d, u, p = test()
+            f.write('{},{},{}\n'.format(d, u, p))
+    # pretty write to txt file
+    with open('file.txt', 'w') as f:
+        for i in range(3):
